5a5d05e1aab8b68a6da849abf62b818c68c30ad9
CongLe <longchike@gmail.com>
2023-12-07 12:04:25 -0800

    - Initial setup

---
+++ AppleMusicV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMusicV2
+//
+//  Created by CONG LE on 12/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
