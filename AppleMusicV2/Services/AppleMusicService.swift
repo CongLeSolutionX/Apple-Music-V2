58bd7144cfb14956c38d818a27cf94e4404e4ab1
CongLe <longchike@gmail.com>
2024-01-17 00:06:35 -0500

    - helper function pretty print the json data received from the server

--- AppleMusicV2/Services/AppleMusicService.swift
+++ AppleMusicV2/Services/AppleMusicService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class AppleMusicService {
-
+    
     // URLSession is used to create a network request.
     func fetchAppleMusic(completion: @escaping (AppleMusic?, Error?) -> Void) {
         // Create a URL from the given string.
@@ -16,27 +16,24 @@ class AppleMusicService {
             completion(nil, URLError(.badURL))
             return
         }
-
+        
         // Create a data task with the URL.
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
             // If an error occurred, call the completion handler with the error.
             if let error = error {
                 completion(nil, error)
                 return
             }
-
+            
             // Check if response data exists.
             guard let data = data else {
                 completion(nil, URLError(.badServerResponse))
                 return
             }
-
+            
             do {
                 // print out the json data structure we received
-                if let jsonString = String(data: data, encoding: .utf8) {
-                    print("Fetched JSON string: \(jsonString)")
-                }
-                
+                self?.prettyPrintJson(data: data)
                 // Decode the received data
                 let fetchedData = try JSONDecoder().decode(AppleMusic.self, from: data)
                 // Call completion handler with the decoded country data on success.
@@ -59,8 +56,17 @@ class AppleMusicService {
                 completion(nil, error)
             }
         }
-
+        
         // Start the network request.
         task.resume()
+    }
+    
+    private func prettyPrintJson(data: Data) {
+        // print out the json data structure we received
+        if let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+           let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted]),
+           let prettyPrintedString = String(data: jsonData, encoding: .utf8) {
+            print("Fetched JSON string:\n\(prettyPrintedString)")
+        }
     }
 }
